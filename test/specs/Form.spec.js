edb054de9cedecc8482fef903c3aa433e6f6b216
Leonardo Silva <138936639+Leonardo-JSilva@users.noreply.github.com>
2023-12-11 18:54:48 -0300

    Update Form.spec.js

--- test/specs/Form.spec.js
+++ test/specs/Form.spec.js
@@ -11,7 +11,7 @@ describe('Test Forms', async () =>{
         await form.turnSwitch()
         await form.selectOpt(5)
         await form.pushBtn()
-        await $('//android.widget.CheckedTextView[@resource-id="android:id/text1" and @text="This app is awesome"]').click()
+        expect (await formScreen.getDropdownResult()).toEqual('webdriver.io is awesome')
 
      });
 })
